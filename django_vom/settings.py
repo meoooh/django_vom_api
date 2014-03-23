4b84029070facbe3ca1eb885c99838b61ceefe96
han <hgkim@move.is>
2014-03-23 14:17:17 +0900

    cors

--- django_vom/settings.py
+++ django_vom/settings.py
@@ -69,6 +69,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 )
 
 ROOT_URLCONF = 'django_vom.urls'
